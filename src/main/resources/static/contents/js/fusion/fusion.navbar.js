cce3df1085eaeeba9477533ad76c723ca544a362
mainul <mainul@bitmascot.com>
2019-02-12 14:52:58 +0600

    adding dynamic nav bar item done

--- src/main/resources/static/contents/js/fusion/fusion.navbar.js
+++ src/main/resources/static/contents/js/fusion/fusion.navbar.js
@@ -1,30 +1,39 @@
 Fusion.appBody.navbar = (function () {
-    return {
-        items: [
-            {
-                label: "Home",
-                href: "#"
-            },
-            {
-                label: "Link",
-                href: "#"
-            },
-        ],
-        initialize: function () {
 
+    var items = []
+    return {
+        initialize: function () {
+            var navContent = Fusion.appBody.navbar.nav.content
+            navContent.innerHTML = ''
+            items = [
+                {
+                    label: "Home",
+                    href: "#"
+                },
+                {
+                    label: "Link",
+                    href: "#"
+                },
+            ]
+            var element = ''
+            items.forEach(function (item) {
+                element = '<li class="nav-item">'
+                element += `<a class="nav-link" href="${item.href}">${item.label}</a>`
+                element += '</li>'
+            })
+            navContent.innerHTML = element
         },
         nav: {
             content: document.querySelector(".navbar-nav"),
             addItem: function (item) {
+                var navContent = Fusion.appBody.navbar.nav.content
+                items.push(item)
                 var element = ''
-                this.items.forEach(function (item) {
-                    element = '<li class="nav-item">'
-                    element += `<a class="nav-link" href="${item.href}">${item.label}</a>`
-                    element += '</li>'
-                })
-                this.navbar.nav.content.innerHTML += element
+                element = '<li class="nav-item">'
+                element += `<a class="nav-link" href="${item.href}">${item.label}</a>`
+                element += '</li>'
+                navContent.innerHTML += element
             }
         }
-
     }
 }());
